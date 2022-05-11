f24eb20ce099619c891974dd4bae540e938460e1
pkzrnvch <pkazarnovich@gmail.com>
2022-05-11 09:34:13 +0500

    Add frontend

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index_page(request):
-    return render(request, 'index_page.html')
+def index(request):
+    return render(request, 'index.html')
