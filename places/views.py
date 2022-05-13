4c11bc29b8f7fa1bbfbc27103ffd3735e0b9f26a
pkzrnvch <pkazarnovich@gmail.com>
2022-05-13 23:23:58 +0500

    Add order_by to place_photos in json API

--- places/views.py
+++ places/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import get_object_or_404
 
 def place_details_json(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
-    place_photos = [photo.image.url for photo in place.photos.all()]
+    place_photos = place.photos.all().order_by('number')
+    place_photos_urls = [photo.image.url for photo in place_photos]
     place_details = {
         'title': place.title,
-        'imgs': place_photos,
+        'imgs': place_photos_urls,
         'description_short': place.short_description,
         'description_long': place.long_description,
         'coordinates': {
